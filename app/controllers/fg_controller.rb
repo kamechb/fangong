a03ae62f5829f92d9ec13244d0135dd6d6d72c38
kame <chenhb2@qqw.com.cn>
2012-06-20 15:58:42 +0800

    fg controller

--- app/controllers/fg_controller.rb
+++ app/controllers/fg_controller.rb
@@ -2,14 +2,14 @@
 class FgController < ApplicationController
   # 定义通用的 page action
   {
-    contact_us: '联系我们',
-    declare: '凡宫声明',
-    enviroment: '环境',
-    partner_school: '合作伙伴',
-    preferential: '学费优惠政策',
-    privacy: '隐私声明',
-    registration_instruction: '报名须知',
-    statement: '商标声明'
+    contact_us:                  '联系我们',
+    declare:                     '凡宫声明',
+    enviroment:                  '环境',
+    partner_school:              '合作伙伴',
+    preferential:                '学费优惠政策',
+    privacy:                     '隐私声明',
+    registration_instruction:    '报名须知',
+    statement:                   '商标声明'
   }.each do |page, slug|
     define_method(page) do
       @page = ::Refinery::Page.where(:slug => slug).first || ::Refinery::Page.where(:link_url => "/#{page}").first
@@ -23,8 +23,6 @@ class FgController < ApplicationController
 
   def about_us
     @page = ::Refinery::Page.where(:slug => "关于我们").first
-    puts '-----------------'
-    p @page
   end
 
   def certificate
